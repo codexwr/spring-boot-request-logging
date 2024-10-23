b9df695068e1c21aac9a4273ecb01961ea3b037b
codexwr <yckim120.dev@gmail.com>
2024-10-23 16:31:50 +0900

    servletRequest FORM 데이터 로깅 처리

--- src/main/java/com/github/codexwr/springbootrequestlogging/servlet/CachedRequestWrapper.java
+++ src/main/java/com/github/codexwr/springbootrequestlogging/servlet/CachedRequestWrapper.java
@@ -1,15 +1,27 @@
 package com.github.codexwr.springbootrequestlogging.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartResolver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 class CachedRequestWrapper extends HttpServletRequestWrapper implements LoggingWrapper {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     private CachedInputStream cachedInputStream = null;
     private BufferedReader bufferedReader = null;
 
@@ -51,13 +63,37 @@ class CachedRequestWrapper extends HttpServletRequestWrapper implements LoggingW
         return isCompatibleMediaType(MediaType.APPLICATION_JSON);
     }
 
-    public byte[] getCachedContentBody() {
-        if (!isCachedBody())
-            return null;
+    private byte[] getInputContent() throws Exception {
+        if (isCompatibleMediaType(MediaType.APPLICATION_FORM_URLENCODED))
+            return objectMapper.writeValueAsString(getRequest().getParameterMap()).getBytes(getCharacterEncoding());
 
+        if (isCompatibleMediaType(MediaType.MULTIPART_FORM_DATA)) {
+            WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getRequest().getServletContext());
+            assert context != null;
+            MultipartResolver multipartResolver = context.getBean(MultipartResolver.class);
+            MultipartHttpServletRequest multipart = multipartResolver.resolveMultipart((HttpServletRequest) getRequest());
+            Map<String, Object> content = new HashMap<>(multipart.getParameterMap().size() + 1);
+            content.putAll(multipart.getParameterMap());
+            multipart.getMultiFileMap().forEach((key, value) -> {
+                @SuppressWarnings("unchecked")
+                var fileNames = (LinkedHashMap<String, List<String>>) content.computeIfAbsent("fileNames", k -> new LinkedHashMap<>());
+                fileNames.put(key, value.stream().map(MultipartFile::getOriginalFilename).toList());
+            });
+
+            return objectMapper.writeValueAsString(content).getBytes(getCharacterEncoding());
+        }
+
+        return null;
+    }
+
+
+    public byte[] getCachedContentBody() {
         try {
+            if (!isCachedBody())
+                return getInputContent();
+
             return getCachedInputStream().getCachedBuffer();
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.trace("Failed to get cached content body", e);
             return null;
         }
